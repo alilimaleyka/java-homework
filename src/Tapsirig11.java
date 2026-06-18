5a0f846679c606b6c578d59eb280c06da13a1d80
Maleyka Alili <alilimaleyka@gmail.com>
2026-06-18 14:28:44 -0700

    Tapşırıq 11 həll edildi

---
+++ src/Tapsirig11.java
@@ -0,0 +1,16 @@
+public class Tapsirig11 {
+    public static void main(String[] args) {
+
+int x = 18;
+int y = 18;
+int z = 25;
+
+System.out.println("x == y → " + (x == y));
+System.out.println("x == z → " + (x == z));
+System.out.println("x != z → " + (x != z));
+System.out.println("z > x → " + (z > x));
+System.out.println("x >= y → " + (x >= y));
+System.out.println("y <= z → " + (y <= z));
+
+    }
+}
