fa62493fab7b5663d98d3b2889b243f9fcaf3630
Maleyka Alili <alilimaleyka@gmail.com>
2026-06-18 12:34:28 -0700

    Tapşırıq 5 həll edildi

---
+++ src/Tapsirig5.java
@@ -0,0 +1,18 @@
+public class Tapsirig5 {
+    public static void main(String[] args) {
+
+        char bashHerf = 'L';
+        String tamAd = "Ləman Əliyeva";
+        int yas = 27;
+        double ayliqGelir = 520.20;
+        boolean vipMusteriMi = false;
+        int musteriKodu = 20027;
+
+        System.out.println("Adının baş hərfi: " + bashHerf);
+        System.out.println("Tam adı: " + tamAd);
+        System.out.println("Yaşı: " + yas);
+        System.out.println("Aylıq gəliri: " + ayliqGelir + " AZN");
+        System.out.println("VIP müştəri mi?: " + vipMusteriMi);
+        System.out.println("Müştəri kodu: " + musteriKodu);
+    }
+}
