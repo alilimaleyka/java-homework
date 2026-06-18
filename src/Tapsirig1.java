ca3cc5ff7cf822f09af440be27fd01b769bf1951
Maleyka Alili <alilimaleyka@gmail.com>
2026-06-18 11:59:41 -0700

    Tapşırıq 1 həll edildi

---
+++ src/Tapsirig1.java
@@ -0,0 +1,16 @@
+public class Tapsirig1 {
+    public static void main(String[] args) {
+
+        String musteriAdi = "Ləman Əliyeva";
+        int hesabNomresi = 123456;
+        double balans = 3842.50;
+        boolean hesabAktivMi = true;
+        String hesabNovu = "Əmanət";
+
+        System.out.println("Müştəri adı     : " + musteriAdi);
+        System.out.println("Hesab nömrəsi   : " + hesabNomresi);
+        System.out.println("Balans          : " + balans + " AZN");
+        System.out.println("Hesab aktiv mi? : " + hesabAktivMi);
+        System.out.println("Hesab növü      : " + hesabNovu);
+    }
+}
