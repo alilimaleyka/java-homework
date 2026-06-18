86ec5cd58acd10271694c9ca7a31fd7e0fca71c7
Maleyka Alili <alilimaleyka@gmail.com>
2026-06-18 14:19:43 -0700

    Tapşırıq 10 həll edildi

---
+++ src/Tapsirig10.java
@@ -0,0 +1,25 @@
+public class Tapsirig10 {
+    public static void main(String[] args) {
+
+    int umumiDeqiqe = 145;
+    int saatBolmeile = umumiDeqiqe / 60;
+    int saatFaizqaileQalanDeqiqe = umumiDeqiqe % 60;
+
+    System.out.println( "145 dəqiqə = " + saatBolmeile + " saat " + saatFaizqaileQalanDeqiqe + " dəqiqə");
+
+
+    umumiDeqiqe = 90;
+    saatBolmeile = umumiDeqiqe / 60;
+    saatFaizqaileQalanDeqiqe = umumiDeqiqe % 60;
+
+    System.out.println( "90 dəqiqə = " + saatBolmeile + " saat " + saatFaizqaileQalanDeqiqe + " dəqiqə");
+
+
+    umumiDeqiqe = 200;
+    saatBolmeile = umumiDeqiqe / 60;
+    saatFaizqaileQalanDeqiqe = umumiDeqiqe % 60;
+
+    System.out.println( "200 dəqiqə = " + saatBolmeile + " saat " + saatFaizqaileQalanDeqiqe + " dəqiqə");
+
+    }
+}
